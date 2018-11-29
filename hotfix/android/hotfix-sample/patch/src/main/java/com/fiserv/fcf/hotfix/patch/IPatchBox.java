70cea50595c38e17b999cc281237bdc9cf41bb24
yangjiang <jiangyang5157@gmail.com>
2018-11-29 18:13:27 +1300

    del

--- hotfix/android/hotfix-sample/patch/src/main/java/com/fiserv/fcf/hotfix/patch/IPatchBox.java
+++
@@ -1,9 +0,0 @@
-package com.fiserv.fcf.hotfix.patch;
-
-import java.util.List;
-
-public interface IPatchBox {
-
-    public List<String> getPatchClasses();
-
-}
--- hotfix/android/hotfix-sample/patch/src/main/java/com/fiserv/fcf/hotfix/patch/Patch.java
+++
@@ -1,7 +0,0 @@
-package com.fiserv.fcf.hotfix.patch;
-
-public interface Patch {
-
-    public Object dispatchMethod(Object host, String methodSign, Object[] params);
-
-}
